ae3f3d200a25031bef0430c3b2186a8d084850c3
DeVerl Stokes <deverl.stokes@gmail.com>
2019-04-06 07:56:18 -0600

    1. Changed the title. 2. Adding some padding to the body. 3. Made the h1 heading vertically centered with the logo. 4. Removed the old loader gif.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get("/", function(req, res, next) {
-  res.render("index", { title: "Oauth Management" });
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Oauth Tester' });
 });
 
 module.exports = router;
