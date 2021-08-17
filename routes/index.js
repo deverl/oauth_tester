db0b73af0034cdeecc36520487b40f34a66285c7
DeVerl Stokes <deverl.stokes@gmail.com>
2021-08-17 12:45:24 -0600

    Modified to have separate text for title and header text.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Oauth Tester for Quickbooks Time' });
+    res.render('index', { title: 'Oauth Tester', header_text: 'Oauth Tester for Quickbooks Time' });
 });
 
 module.exports = router;
